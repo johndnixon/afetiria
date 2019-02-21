4a28fee3fec12ff66199514c525a2ee8872a9583
John Nixon <john.nixon@eightohtwo.com>
2019-02-21 09:54:12 -0500

    Update class-afp-register-taxonomies.php

--- includes/class-afp-register-taxonomies.php
+++ includes/class-afp-register-taxonomies.php
@@ -15,44 +15,52 @@ class AFP_Register_Taxonomies {
      * Initialize the class
      */
     public function __construct() {
-        add_action( 'init', array( $this, 'register_gallerytags_taxonomy' ) );
+        add_action( 'init', array( $this, 'register_taxonomies' ) );
     }
 
-    /**
-     * Register Gallerytags Taxonomy
-     *
-     * @since  1.0.0
-     * @access public
-     * @return void
-     */
-    public function register_gallerytags_taxonomy() {
-        $gallery_tag_labels = array(
-            'name'                       => _x( 'Gallery Tags', 'taxonomy general name' ),
-            'singular_name'              => _x( 'Gallery Tag', 'taxonomy singular name' ),
-            'search_items'               => __( 'Search Gallery Tags' ),
-            'popular_items'              => __( 'Popular Gallery Tags' ),
-            'all_items'                  => __( 'All Gallery Tags' ),
-            'parent_item'                => null,
-            'parent_item_colon'          => null,
-            'edit_item'                  => __( 'Edit Gallery Tag' ),
-            'update_item'                => __( 'Update Gallery Tag' ),
-            'add_new_item'               => __( 'Add New Gallery Tag' ),
-            'new_item_name'              => __( 'New Gallery Tag' ),
-            'separate_items_with_commas' => __( 'Separate gallery tags with commas' ),
-            'add_or_remove_items'        => __( 'Add or remove gallery tags' ),
-            'choose_from_most_used'      => __( 'Choose from the most used gallery tags' ),
-            'menu_name'                  => __( 'Gallery Tags' ),
+
+    function register_taxonomies(  ) {
+        // $this->set_up_taxonomy(
+        //     array( 'Plural', 'Singular', 'tax-slug', 'tax-key' ),
+        //     array( 'hierarchical' => true )
+        // );
+    }
+
+    function set_up_taxonomy( $labelSettings = array(), $args = array() ) {
+
+        $labels = array(
+            'name'                       => _x( $labelSettings[0], 'Taxonomy General Name', 'afp' ),
+            'singular_name'              => _x( $labelSettings[1], 'Taxonomy Singular Name', 'afp' ),
+            'menu_name'                  => __( $labelSettings[0], 'afp' ),
+            'all_items'                  => __( 'All ' . $labelSettings[1], 'afp' ),
+            'parent_item'                => __( 'Parent ' . $labelSettings[1], 'afp' ),
+            'parent_item_colon'          => __( 'Parent Speaker:', 'afp' ),
+            'new_item_name'              => __( 'New Speaker Name', 'afp' ),
+            'add_new_item'               => __( 'Add New ' . $labelSettings[1], 'afp' ),
+            'edit_item'                  => __( 'Edit ' . $labelSettings[1], 'afp' ),
+            'update_item'                => __( 'Update ' . $labelSettings[1], 'afp' ),
+            'view_item'                  => __( 'View ' . $labelSettings[1], 'afp' ),
+            'separate_items_with_commas' => __( 'Separate speakers with commas', 'afp' ),
+            'add_or_remove_items'        => __( 'Add or remove ' . $labelSettings[1], 'afp' ),
+            'choose_from_most_used'      => __( 'Choose from the most used', 'afp' ),
+            'popular_items'              => __( 'Popular ' . $labelSettings[1], 'afp' ),
+            'search_items'               => __( 'Search ' . $labelSettings[1], 'afp' ),
+            'not_found'                  => __( 'Not Found', 'afp' ),
+            'no_terms'                   => __( 'No ' . $labelSettings[1], 'afp' ),
+            'items_list'                 => __( 'Speakers list', 'afp' ),
+            'items_list_navigation'      => __( 'Speakers list navigation', 'afp' ),
         );
-        register_taxonomy( "gallerytags", array( "screenshot" ), array(
-                'labels'       => $gallery_tag_labels,
-                'show_ui'      => true,
-                'query_var'    => true,
-                'rewrite'      => array(
-                'slug'         => 'gallery/tag',
-                'with_front'   => true,
-                'heirarchical' => false
-                )
-            )
-        );
+        $args = wp_parse_args( $args, array(
+            'labels'                     => $labels,
+            'hierarchical'               => false,
+            'public'                     => true,
+            'show_ui'                    => true,
+            'show_admin_column'          => true,
+            'show_in_nav_menus'          => true,
+            'show_tagcloud'              => true,
+            'show_in_rest'               => true,
+        ));
+        register_taxonomy( $labelSettings[2], array( $labelSettings[3] ), $args );
+
     }
 }
