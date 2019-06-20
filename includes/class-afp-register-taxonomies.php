0b929ebcd480591e8a507bb9f7d8483b929eb912
John Nixon <johndnixon@users.noreply.github.com>
2019-06-20 08:09:53 -0400

    Update class-afp-register-taxonomies.php

--- includes/class-afp-register-taxonomies.php
+++ includes/class-afp-register-taxonomies.php
@@ -1,4 +1,34 @@
-$labels = array(
+<?php
+/**
+ * Register Custom Taxonomies
+ *
+ * @package     AFP
+ * @subpackage  AFP/includes
+ * @copyright   Copyright (c) 2019, John Nixon
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.0.0
+ */
+
+class AFP_Register_Taxonomies {
+
+    /**
+     * Initialize the class
+     */
+    public function __construct() {
+        add_action( 'init', array( $this, 'register_taxonomies' ) );
+    }
+
+
+    function register_taxonomies(  ) {
+       /* $this->set_up_taxonomy(
+            array( 'Plural', 'Singular', 'slug', 'post type key' ),
+            array( 'hierarchical' => true )
+        ); */
+    }
+
+    function set_up_taxonomy( $labelSettings = array(), $args = array() ) {
+
+        $labels = array(
             'name'                       => _x( $labelSettings[0], 'Taxonomy General Name', 'afp' ),
             'singular_name'              => _x( $labelSettings[1], 'Taxonomy Singular Name', 'afp' ),
             'menu_name'                  => __( $labelSettings[0], 'afp' ),
@@ -20,3 +50,17 @@ $labels = array(
             'items_list'                 => __( $labelSettings[0] . ' list', 'afp' ),
             'items_list_navigation'      => __( $labelSettings[0] . ' list navigation', 'afp' ),
         );
+        $args = wp_parse_args( $args, array(
+            'labels'                     => $labels,
+            'hierarchical'               => false,
+            'public'                     => true,
+            'show_ui'                    => true,
+            'show_admin_column'          => true,
+            'show_in_nav_menus'          => true,
+            'show_tagcloud'              => true,
+            'show_in_rest'               => true,
+        ));
+        register_taxonomy( $labelSettings[2], array( $labelSettings[3] ), $args );
+
+    }
+}
