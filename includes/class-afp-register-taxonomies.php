23f9c6cae4e1acdd10c9f00ade44df583a7c7be5
John Nixon <johndnixon@users.noreply.github.com>
2019-06-20 08:09:04 -0400

    Update class-afp-register-taxonomies.php

--- includes/class-afp-register-taxonomies.php
+++ includes/class-afp-register-taxonomies.php
@@ -1,41 +1,11 @@
-<?php
-/**
- * Register Custom Taxonomies
- *
- * @package     AFP
- * @subpackage  AFP/includes
- * @copyright   Copyright (c) 2019, John Nixon
- * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
- * @since       1.0.0
- */
-
-class AFP_Register_Taxonomies {
-
-    /**
-     * Initialize the class
-     */
-    public function __construct() {
-        add_action( 'init', array( $this, 'register_taxonomies' ) );
-    }
-
-
-    function register_taxonomies(  ) {
-        // $this->set_up_taxonomy(
-        //     array( 'Plural', 'Singular', 'tax-slug', 'post type key' ),
-        //     array( 'hierarchical' => true )
-        // );
-    }
-
-    function set_up_taxonomy( $labelSettings = array(), $args = array() ) {
-
-        $labels = array(
+$labels = array(
             'name'                       => _x( $labelSettings[0], 'Taxonomy General Name', 'afp' ),
             'singular_name'              => _x( $labelSettings[1], 'Taxonomy Singular Name', 'afp' ),
             'menu_name'                  => __( $labelSettings[0], 'afp' ),
             'all_items'                  => __( 'All ' . $labelSettings[1], 'afp' ),
             'parent_item'                => __( 'Parent ' . $labelSettings[1], 'afp' ),
-            'parent_item_colon'          => __( 'Parent Speaker:', 'afp' ),
-            'new_item_name'              => __( 'New Speaker Name', 'afp' ),
+            'parent_item_colon'          => __( 'Parent ' . $labelSettings[1] . ':', 'afp' ),
+            'new_item_name'              => __( 'New ' . $labelSettings[1] . '', 'afp' ),
             'add_new_item'               => __( 'Add New ' . $labelSettings[1], 'afp' ),
             'edit_item'                  => __( 'Edit ' . $labelSettings[1], 'afp' ),
             'update_item'                => __( 'Update ' . $labelSettings[1], 'afp' ),
@@ -50,17 +20,3 @@ class AFP_Register_Taxonomies {
             'items_list'                 => __( $labelSettings[0] . ' list', 'afp' ),
             'items_list_navigation'      => __( $labelSettings[0] . ' list navigation', 'afp' ),
         );
-        $args = wp_parse_args( $args, array(
-            'labels'                     => $labels,
-            'hierarchical'               => false,
-            'public'                     => true,
-            'show_ui'                    => true,
-            'show_admin_column'          => true,
-            'show_in_nav_menus'          => true,
-            'show_tagcloud'              => true,
-            'show_in_rest'               => true,
-        ));
-        register_taxonomy( $labelSettings[2], array( $labelSettings[3] ), $args );
-
-    }
-}
